038efc0c825774c535c1cd726440d5cadc74c03b
Swatej Patil <81546709+SwatejPatil@users.noreply.github.com>
2022-08-11 17:28:27 +0530

    Update gauge.component.ts

--- src/app/gauge/gauge.component.ts
+++ src/app/gauge/gauge.component.ts
@@ -10,81 +10,33 @@ export class GaugeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.createGauge();
   }
 
-  createGauge(){
-   
-    // var  V_LL = this.ChartData[0].V_LL;
-    // V_LL =   V_LL/1000;
+  createGauge() {
+    setInterval(function () {
+      const gaugeElement = document.querySelector(".gauge");
 
-    // var  KVA = this.ChartData[0].KVA;
-    //           KVA =   KVA/1000;
+      function setGaugeValue(gauge: any, value: number) {
+        if (value < 0 || value > 1) {
+          return;
+        }
 
+        gauge.querySelector(".niddle").style.transform = `rotate(${value / 2
+          }turn)`;
+        gauge.querySelector(".gaugeCover").textContent = `${Math.round(
+          value * 100
+        )}`;
+      }
 
-      // console.log(KVA);
+      var random = Math.random();
+      console.log(random)
 
+      setGaugeValue(gaugeElement, random);
 
-      // console.log(Number(V_LL));
-      
+    }, 2000);
 
-
-        setInterval(function(){
-
-
-          const gaugeElement = document.querySelector(".gauge");
-
-          function setGaugeValue(gauge: any, value: number) {
-            if (value < 0 || value > 0.45) {
-              return;
-            }
-
-            gauge.querySelector(".gauge__fill").style.transform = `rotate(${
-              value / 0.93
-            }turn)`;
-            gauge.querySelector(".gauge__cover").textContent = `${Math.round(
-              value * 1000
-            )}`;
-          }
-
-          var random = Math.random();
-        // setGaugeValue(gaugeElement, Number(V_LL));
-
-        setGaugeValue(gaugeElement, random);
-
-        }, 1000);  
-        
-        
-
-        
-      
-
-
-        setInterval(function(){
-          
-
-          const gaugeElement1 = document.querySelector(".gauge1");
-
-          function setGaugeValue(gauge: any, value: number) {
-            if (value < 0 || value > 0.7) {
-              return;
-            }
-
-            gauge.querySelector(".gauge__fill1").style.transform = `rotate(${
-              value / 1.5
-            }turn)`;
-            gauge.querySelector(".gauge__cover1").textContent = `${Math.round(
-              value * 1000
-            )}`;
-          }
-
-          var random = Math.random();
-          console.log(random);
-          
-        // setGaugeValue(gaugeElement1, Number(KVA));
-        setGaugeValue(gaugeElement1, random);
-
-        }, 1000); 
-}
+  }
 
 
 }
